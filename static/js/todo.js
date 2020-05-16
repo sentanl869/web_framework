21f7dff5bebebf5192ddf9982f2635ef8320a3be
sentanl869 <sentanl869@gmail.com>
2020-05-16 21:44:16 +0800

    Add ajax todo edit

--- static/js/todo.js
+++ static/js/todo.js
@@ -1,3 +1,22 @@
+let todoTemplate = function (todo) {
+    return`
+        <div class="todo-cell" data-id="${todo.id}">
+            <span class="todo-title">${todo.title}</span>
+            <button class="todo-edit">编辑</button>
+            <button class="todo-delete">完成</button>
+        </div>
+    `;
+};
+
+let todoEditTemplate = function (title) {
+    return `
+        <div class="todo-update-form">
+            <input class="todo-update-input" value="${title}"/>
+            <button class="todo-update">更新</button>
+        </div>
+    `;
+};
+
 let apiTodoAll = function (callback) {
     let path = '/api/todo/all';
     ajax('GET', path, '', callback);
@@ -13,20 +32,15 @@ let apiTodoDelete = function (todoId, callback) {
     ajax('GET', path, '', callback);
 };
 
-let todoTemplate = function (todo) {
-    return`
-        <div class="todo-cell" data-id="${todo.id}">
-            <span class="todo-title">${todo.title}</span>
-            <button class="todo-edit">编辑</button>
-            <button class="todo-delete">完成</button>
-        </div>
-    `;
-};
-
 let insertTodo = function (todo) {
     let todoCell = todoTemplate(todo);
     let todoList = document.querySelector('#id-todo-list');
     todoList.insertAdjacentHTML('beforeend', todoCell);
+};
+
+let insertEditForm = function (title, todoCell) {
+    let editForm = todoEditTemplate(title);
+    todoCell.insertAdjacentHTML('beforeend', editForm)
 };
 
 let loadTodos = function () {
@@ -65,9 +79,24 @@ let bindEventTodoDelete = function () {
     });
 };
 
+let bindEventTodoEdit = function () {
+    let bind = document.querySelector('#id-todo-list');
+    bind.addEventListener('click', function (event) {
+        let self = event.target;
+        if (self.classList.contains('todo-edit')) {
+            let todoCell = self.closest('.todo-cell');
+            let todoSpan = todoCell.querySelector('.todo-title');
+            let title = todoSpan.innerText;
+            insertEditForm(title, todoCell);
+            self.disabled = true;
+        }
+    });
+};
+
 let bindEventTodo = function () {
     bindEventTodoAdd();
     bindEventTodoDelete();
+    bindEventTodoEdit();
 };
 
 let __main = function () {
