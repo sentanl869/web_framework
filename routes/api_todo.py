19d8a3ad65f0d5b6822f6a8d5e79e5dc75d456f9
sentanl869 <sentanl869@gmail.com>
2020-05-16 19:50:48 +0800

    Add ajax todo add and delete

--- routes/api_todo.py
+++ routes/api_todo.py
@@ -4,6 +4,7 @@ from routes import (
     login_required,
 )
 from models.todo import Todo
+# from utiles import log
 
 
 @login_required
@@ -13,8 +14,26 @@ def api_all(request) -> bytes:
     return json_response(todos)
 
 
+@login_required
+def api_add(request) -> bytes:
+    form = request.json()
+    u = current_user(request)
+    t = Todo.add(form, u.id)
+    return json_response(t.json())
+
+
+@login_required
+def api_delete(request) -> bytes:
+    todo_id = request.query['id']
+    Todo.delete(todo_id)
+    r = dict()
+    return json_response(r)
+
+
 def route_dict() -> dict:
     d = {
         '/api/todo/all': api_all,
+        '/api/todo/add': api_add,
+        '/api/todo/delete': api_delete,
     }
     return d
--- static/js/todo.js
+++ static/js/todo.js
@@ -3,33 +3,71 @@ let apiTodoAll = function (callback) {
     ajax('GET', path, '', callback);
 };
 
-let todoTemplate = function (todo, num) {
+let apiTodoAdd = function (form, callback) {
+    let path = '/api/todo/add';
+    ajax('POST', path, form, callback);
+};
+
+let apiTodoDelete = function (todoId, callback) {
+    let path = `/api/todo/delete?id=${todoId}`;
+    ajax('GET', path, '', callback);
+};
+
+let todoTemplate = function (todo) {
     return`
         <div class="todo-cell" data-id="${todo.id}">
-            <span class="todo-title">${num} : ${todo.title}</span>
+            <span class="todo-title">${todo.title}</span>
             <button class="todo-edit">编辑</button>
-            <button class="todo-delete">删除</button>
+            <button class="todo-delete">完成</button>
         </div>
     `;
 };
 
-let insertTodo = function (todo, num = null) {
-    let todoCell = todoTemplate(todo, num);
+let insertTodo = function (todo) {
+    let todoCell = todoTemplate(todo);
     let todoList = document.querySelector('#id-todo-list');
-    todoList.insertAdjacentHTML('beforeend', todoCell)
+    todoList.insertAdjacentHTML('beforeend', todoCell);
 };
 
 let loadTodos = function () {
     apiTodoAll(function (todos) {
         for (let i = 0; i < todos.length; i++) {
-            let todo = todos[i]
-            insertTodo(todo, i + 1)
+            let todo = todos[i];
+            insertTodo(todo);
+        }
+    });
+};
+
+let bindEventTodoAdd = function () {
+    let bind = document.querySelector('#id-button-add');
+    bind.addEventListener('click', function () {
+        let input = document.querySelector('#id-input-todo');
+        let title = input.value;
+        let form = {
+            title: title
+        };
+        apiTodoAdd(form, function (todo) {
+            insertTodo(todo);
+        });
+    });
+};
+
+let bindEventTodoDelete = function () {
+    let bind = document.querySelector('#id-todo-list');
+    bind.addEventListener('click', function (event) {
+        let self = event.target;
+        if (self.classList.contains('todo-delete')) {
+            let todoId = self.parentElement.dataset['id'];
+            apiTodoDelete(todoId, function () {
+                self.parentElement.remove();
+            });
         }
     });
 };
 
 let bindEventTodo = function () {
-
+    bindEventTodoAdd();
+    bindEventTodoDelete();
 };
 
 let __main = function () {
