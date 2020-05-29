3216b0c8454a07f2de5c17d302b4dc3974d2b024
sentanl869 <sentanl869@gmail.com>
2020-05-30 00:18:24 +0800

    Improve log

--- utiles.py
+++ utiles.py
@@ -1,2 +1,15 @@
+from time import (
+    localtime,
+    strftime,
+)
+
+
+def formatted_time() -> str:
+    time_format = '%Y/%m/%d %H:%M:%S'
+    value = localtime()
+    formatted = strftime(time_format, value)
+    return formatted
+
+
 def log(*args, **kwargs):
-    print(*args, **kwargs)
+    print(formatted_time(), *args, **kwargs)
