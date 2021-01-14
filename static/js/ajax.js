6e519bfc26256a1e1b41d50f8c74d8ac5e011bbf
sentanl869 <sentanl869@gmail.com>
2021-01-14 19:46:42 +0800

    Improve ajax.js

--- static/js/ajax.js
+++ static/js/ajax.js
@@ -1,14 +1,14 @@
 let ajax = function (method, path, data, token, responseCallback) {
-    let r = new XMLHttpRequest();
-    r.open(method, path, true);
-    r.setRequestHeader('Content-Type', 'application/json');
-    r.setRequestHeader('X-CSRF-TOKEN', token);
-    r.onreadystatechange = function () {
-        if (r.readyState === 4) {
-            let json = JSON.parse(r.response);
+    let request = new XMLHttpRequest();
+    request.open(method, path, true);
+    request.setRequestHeader('Content-Type', 'application/json');
+    request.setRequestHeader('X-CSRF-TOKEN', token);
+    request.onreadystatechange = function () {
+        if (request.readyState === 4) {
+            let json = JSON.parse(request.response);
             responseCallback(json);
         }
     };
     data = JSON.stringify(data);
-    r.send(data);
+    request.send(data);
 };
